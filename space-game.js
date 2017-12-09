75fb3aaf98b0523f32436e01fb1d1c953e93e16f
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-12-09 14:09:16 -0500

    Implemented lasers and WASD movement

--- space-game.js
+++ space-game.js
@@ -5,15 +5,39 @@ var graphics;
 var debugText;
 var ACCEL = 20;
 var SPEED = 400;
-var DEACCEL = 20;
+var DEACCEL = 10;
+
+var fireRate = 100;
+var nextFire = 0;
+
+var playerLaserSpeed = 600;
 
 function preload() {
 	game.load.image("TRIANGLE", "img/triangle.png");
+	game.load.image("BEAM", "img/beam.png");
 }
 
 function create() {
 	//init graphics engine
 	graphics = game.add.graphics();
+	
+	//add player lasers
+	playerLasers = game.add.group();
+    playerLasers.enableBody = true;
+    playerLasers.physicsBodyType = Phaser.Physics.ARCADE;
+
+    playerLasers.createMultiple(50, "BEAM");
+    playerLasers.setAll("checkWorldBounds", true);
+    playerLasers.setAll("outOfBoundsKill", true);
+	
+	//add enemy lasers
+	badLasers = game.add.group();
+    badLasers.enableBody = true;
+    badLasers.physicsBodyType = Phaser.Physics.ARCADE;
+
+    badLasers.createMultiple(50, "BEAM");
+    badLasers.setAll("checkWorldBounds", true);
+    badLasers.setAll("outOfBoundsKill", true);
 	
 	//add player
 	player = game.add.sprite(400, 300 , "TRIANGLE");
@@ -31,9 +55,9 @@ function update() {
 	player.rotation = game.physics.arcade.angleToPointer(player);
 	
 	//horizontal velocity
-	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT) || game.input.keyboard.isDown(Phaser.Keyboard.A)) {
 		player.body.velocity.x -= ACCEL;
-	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) || game.input.keyboard.isDown(Phaser.Keyboard.D)) {
 		player.body.velocity.x += ACCEL;
 	} else {
 		player.body.velocity.x += Math.sign(player.body.velocity.x) * -DEACCEL;
@@ -44,9 +68,9 @@ function update() {
 	}
 	
 	//vertical velocity
-	if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+	if (game.input.keyboard.isDown(Phaser.Keyboard.UP) || game.input.keyboard.isDown(Phaser.Keyboard.W)) {
 		player.body.velocity.y -= ACCEL;
-	} else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN) || game.input.keyboard.isDown(Phaser.Keyboard.S)) {
 		player.body.velocity.y += ACCEL;
 	} else {
 		player.body.velocity.y += Math.sign(player.body.velocity.y) * -DEACCEL;
@@ -54,6 +78,11 @@ function update() {
 	//stop sliding
 	if (player.body.velocity.y < 5 && player.body.velocity.y > -5) {
 		player.body.velocity.y = 0;
+	}
+	
+	//fire
+	if (game.input.activePointer.leftButton.isDown) {
+		playerFire();
 	}
 	
 	//draw aim
@@ -70,5 +99,11 @@ function drawPlayerAim() {
 }
 
 function playerFire() {
-	var bullet = game.add.sprite(player.x, player.y, "TRIANGLE");
-	//todo: look at this https://phaser.io/examples/v2/arcade-physics/shoot-the-pointer
+	if (game.time.now > nextFire && playerLasers.countDead() > 0) {
+		nextFire = game.time.now + fireRate;
+		var laser = playerLasers.getFirstDead();
+		laser.reset(player.x, player.y);
+		laser.angle = player.angle;
+		game.physics.arcade.moveToPointer(laser, playerLaserSpeed);
+    }
+}
