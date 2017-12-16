a48d04b609e878705327ae5013f9dbac4142318c
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-12-15 23:40:38 -0500

    added polyps group

--- space-game.js
+++ space-game.js
@@ -76,11 +76,19 @@ function create() {
 	//load turrets
 	loadTurrets(game, player);
 	
-	//add polyp
-	polyp = game.add.sprite(300, 200, "POLYP");
-	anim = polyp.animations.add("idle");
-	anim.enableUpdate = true;
-	polyp.animations.play("idle", 8, true);
+	//add polyps
+	// polyp = game.add.sprite(300, 200, "POLYP");
+	// anim = polyp.animations.add("idle");
+	// anim.enableUpdate = true;
+	// polyp.animations.play("idle", 8, true);
+	polyps = game.add.group();
+	polyps.enableBody = true;
+	polyps.physicsBodyType = Phaser.Physics.ARCADE;
+	polyps.createMultiple(4, "POLYP");
+	polyps.reviveAll();
+	polyps.callAll("animations.add", "animations", "idle", null, 8, true);
+	polyps.callAll("anchor.setTo", "anchor", 0.5, 0.5);
+	polyps.scatter(Phaser.Rectangle(32, 32, 3840-32, 3840-32), true); //these numbers are the world bounds with margin of 32
 	
 	//enemy particles
 	enemyParticles = game.add.emitter(0, 0, 50);
