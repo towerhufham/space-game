16d10311c3a4dce98d37374b3e7790b74f70943a
towerhufham <32599109+towerhufham@users.noreply.github.com>
2018-01-14 13:33:55 -0500

    started zodiac implementation

---
+++ zodiac.js
@@ -0,0 +1,17 @@
+var ARIES = false;
+var TAURUS = false;
+var GEMINI = false;
+var CANCER = false;
+var LEO = false;
+var VIRGO = false;
+var LIBRA = false;
+var SCORPIO = false;
+var SAGITTARIUS = false;
+var CAPRICORN = false;
+var AQUARIUS = false;
+var PISCES = false;
+
+function loadZodiac() {
+	//load
+}
+
