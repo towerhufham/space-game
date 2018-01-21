f9cc59c38c064c43fe1287ce39fdc5ff7dfe115f
towerhufham <32599109+towerhufham@users.noreply.github.com>
2018-01-20 19:35:54 -0500

    drop alerts are properly centered

--- ui.js
+++ ui.js
@@ -149,8 +149,10 @@ function drawMap() {
 function dropAlert(main, sub) {
 	//make text
 	mainText = game.add.text(game.scale.width / 2, -200, main, {font: "64px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
+	mainText.x -= mainText.width / 2;
 	mainText.fixedToCamera = true;
 	subText = game.add.text(game.scale.width / 2, -100, sub, {font: "32px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
+	subText.x -= subText.width / 2;
 	subText.fixedToCamera = true;
 	//move it
 	game.add.tween(mainText.cameraOffset).to({y: 100}, 1000, Phaser.Easing.Exponential.Out, true);
