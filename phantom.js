88bc1be979e21c9be3388bd9884d9cf12e5e4f11
towerhufham <32599109+towerhufham@users.noreply.github.com>
2018-01-21 14:32:23 -0500

    phantoms stop if the parent dies

--- phantom.js
+++ phantom.js
@@ -4,11 +4,13 @@ var PHANTOM_TIME = 500;
 function makePhantom(parent, key) {
 	var phantomTimer = game.time.create(false);
 	phantomTimer.loop(PHANTOM_RATE, function(){
-		var phantom = game.add.sprite(parent.x, parent.y, key);
-		phantom.anchor.setTo(0.5, 0.5);
-		phantom.angle = player.angle;
-		game.add.tween(phantom).to({alpha:0}, PHANTOM_TIME, Phaser.Easing.Exponential.Out, true);
-		game.time.events.add(PHANTOM_TIME, function(){phantom.destroy();}, this);
+		if (parent.alive) {
+			var phantom = game.add.sprite(parent.x, parent.y, key);
+			phantom.anchor.setTo(0.5, 0.5);
+			phantom.angle = player.angle;
+			game.add.tween(phantom).to({alpha:0}, PHANTOM_TIME, Phaser.Easing.Exponential.Out, true);
+			game.time.events.add(PHANTOM_TIME, function(){phantom.destroy();}, this);
+		}
 	}, this);
 	phantomTimer.start();
 	parent.phantomTimer = phantomTimer;
