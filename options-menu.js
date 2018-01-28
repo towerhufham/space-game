d634d23ea2557d64514b3cd1f2e0d6ff00c52d63
towerhufham <32599109+towerhufham@users.noreply.github.com>
2018-01-27 20:28:47 -0500

    added options for toggling sfx and music :musical_note:

--- options-menu.js
+++ options-menu.js
@@ -1,7 +1,7 @@
 var atOptions = false;
 var recentlyPressed = false;
 var optionSelector = 0;
-var totalOptions = 2;
+var totalOptions = 4;
 var recentlyClosed = false;
 
 var menuObjects = [];
@@ -15,15 +15,25 @@ function loadOptionsMenu() {
 	var optionsBG = game.add.tileSprite(0, 0, 3840, 3840, "BACKGROUND");
 	menuObjects.push(optionsBG);
 	
-	var backText = game.add.text(600, 300, "Back", {font: "64px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
+	var backText = game.add.text(600, 100, "Back", {font: "64px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
 	backText.fixedToCamera = true;
 	menuObjects.push(backText);
 	options[0] = backText;
 	
-	var portraitText = game.add.text(600, 500, "Change Portrait", {font: "64px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
+	var portraitText = game.add.text(600, 300, "Change Portrait", {font: "64px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
 	portraitText.fixedToCamera = true;
 	menuObjects.push(portraitText);
 	options[1] = portraitText;
+	
+	var sfxText = game.add.text(600, 500, "Sound effects: on", {font: "64px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
+	sfxText.fixedToCamera = true;
+	menuObjects.push(sfxText);
+	options[2] = sfxText;
+	
+	var musicText = game.add.text(600, 700, "Music: on", {font: "64px Arial", fill:"#FFFFFF", stroke:"#000000", strokeThickness:"10", align:"center"});
+	musicText.fixedToCamera = true;
+	menuObjects.push(musicText);
+	options[3] = musicText;
 	
 	drawPortrait();
 	
@@ -67,7 +77,9 @@ function optionMenuUpdate() {
 				unloadOptionsMenu();
 				atOptions = false;
 				recentlyClosed = true;
-			} else if (optionSelector === 1) {
+			}
+			// 1 is change portrait
+			else if (optionSelector === 1) {
 				if (portraitId < 4) {
 					portraitId++;
 				} else {
@@ -76,6 +88,14 @@ function optionMenuUpdate() {
 				console.log("portrait id changed to " + portraitId);
 				setCookie("portraitId", portraitId);
 				drawPortrait();
+			}
+			// 2 is mute sfx
+			else if (optionSelector === 2) {
+				//mute sfx
+			}
+			// 3 is mute music
+			else if (optionSelector === 3) {
+				//mute music
 			}
 			recentlyPressed = true;
 		}
